54c43cd26dc28c4a176f1af44072b2851e141adb
Felipe de Sousa <felipescoelho517@gmail.com>
2025-04-14 14:55:05 +0000

    inserindo analisador lexico completo

---
+++ projeto/exemplo.ts
@@ -0,0 +1,18 @@
+function main(): number {
+  let x: number = 0;
+  // Single line comment
+  /*
+    Multi
+    Line
+    Comment
+    */
+  x == 1;
+  switch (x) {
+    case 1:
+      break;
+    default:
+      break;
+  }
+  console.log("Hello World!\n");
+  return 0;
+}
